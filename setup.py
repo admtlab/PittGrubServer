cca0a60e26f5e117d0b9ab9933918b6c3fd997c3
Mark Silvis <marksilvis@pitt.edu>
2017-08-04 16:58:47 -0400

    replaces argon2 dependency, adds bcrypt

--- setup.py
+++ setup.py
@@ -17,14 +17,14 @@ setup(
         'pymysql>=0.7.11',
         'sqlalchemy>=1.1.10',
         'passlib>=1.7.1',
-        'argon2_cffi>=16.3.0',
+        'bcrypt>=3.1.3',
         'exponent-server-sdk>=0.1.1',
         'sockjs>=0.6.0',
         'inflect>=0.2.5',
-        'flake8>=3.3.0'
+        #'flake8>=3.3.0'
     ],
     tests_require=[
-        'flake8>=3.3.0',
+        #'flake8>=3.3.0',
         'nose>=1.3.7',
         'mock>=2.0.0',
     ],
