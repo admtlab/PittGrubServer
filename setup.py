48ab92f89a9d2664cfc00162a057d3ee1d6a0557
Mark Silvis <marksilvis@pitt.edu>
2017-08-29 16:53:49 -0400

    includes pillow dependency

--- setup.py
+++ setup.py
@@ -19,6 +19,7 @@ setup(
         'passlib>=1.7.1',
         'bcrypt>=3.1.3',
         'pyjwt>=1.5.2',
+        'pillow>=4.2.1',
         'exponent-server-sdk>=0.1.1',
         'sockjs>=0.6.0',
         'inflect>=0.2.5',
