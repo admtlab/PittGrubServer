96bc47a8ad3602e5eef5f00bd143a61b5cf196df
Mark Silvis <marksilvis@pitt.edu>
2017-10-06 10:12:11 +0000

    simplifies config file

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 today=`date +%Y-%m-%d.%H:%M:%S`
-nohup python3.6 pittgrub/ --config=prod.config.ini > ../logs/$today.log  &
+nohup python3.6 pittgrub/ --config=config.ini > ./logs/$today.log  &
 echo 'PittGrub server is up'
 
