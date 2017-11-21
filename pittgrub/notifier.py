8e57aa470f5f725671370ea90b144364875aaf5a
Mark Silvis <marksilvis@pitt.edu>
2017-11-21 13:38:53 -0500

    separates out user notification code

---
+++ pittgrub/notifier.py
@@ -0,0 +1,37 @@
+"""
+Send notifications to users
+Author: Mark Silvis
+"""
+
+from typing import Any, Dict, Tuple, Union
+
+from exponent_server_sdk import (
+    PushClient, PushMessage, PushServerError, PushResponseError,
+)
+
+
+class InvalidExpoToken(Exception):
+    def __init__(self):
+        self.message = "Invalid expo token"
+        super(ValidationError, self).__init__(self.message)
+
+
+def send_push_notification(expo_token: str,
+                           title: str,
+                           body: str,
+                           data: Dict[Any, Any]=None):
+    """Send notification to specified expo token
+    :expo_token: token to send notificaton to
+    :title: notification title
+    :body: notification body
+    :data: extra notification data (total payload must be under 4096 bytes)
+    :raises: ConnectionError, DeviceNotRegisterdError, HTTPError,
+             InvalidExpoToken, PushServerError, PushResponseError
+    """
+    assert expo_token, "Expo token cannot be None"
+    if PushClient().is_exponent_push_token(expo_token):
+        message = PushMessage(to=expo_token, title=title, body=body, data=data)
+        response = PushClient().publish(message)
+        response.validate_response()
+    else:
+        raise InvalidExpoToken()
