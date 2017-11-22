44cfbd11ead9cd7f7e74729bb9747e1f66773525
Mark Silvis <marksilvis@pitt.edu>
2017-11-22 16:07:36 -0500

    updates event notification data

--- pittgrub/handlers/index.py
+++ pittgrub/handlers/index.py
@@ -45,9 +45,9 @@ def send_push_notification(user: 'User', event: 'Event'):
         if PushClient().is_exponent_push_token(expo_token):
             try:
                 message = PushMessage(to=expo_token,
-                                      title=f'PittGrub: New event!',
-                                      body=f'{event.title}',
-                                      data={'type': 'event', 'event': event.title})
+                                      title='PittGrub: New event!',
+                                      body=event.title',
+                                      data={'type': 'event', 'event': event.title, 'title':'PittGrub: New event!', 'body': event.title})
                 response = PushClient().publish(message)
                 response.validate_response()
             except PushServerError as e:
