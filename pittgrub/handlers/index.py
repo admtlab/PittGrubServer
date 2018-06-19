be7d6839f30fab7f6386c45c34178e7a73a08d0c
Mark Silvis <marksilvis@pitt.edu>
2018-06-19 00:07:55 -0400

    adds newsletter email confirmation

--- pittgrub/handlers/index.py
+++ pittgrub/handlers/index.py
@@ -8,6 +8,7 @@ import logging
 
 from __init__ import __version__
 from db import health_check
+from emailer import send_email_list_confirmation
 from handlers.base import BaseHandler, CORSHandler
 from service.user import add_to_email_list, remove_from_email_list
 from util import json_esc
@@ -19,6 +20,7 @@ class EmailListAddHandler(CORSHandler):
         email = path.replace('/', '')
 
         if email and validate_email(email) and add_to_email_list(email):
+            send_email_list_confirmation(email)
             self.success(200)
         else:
             self.write_error(400)
@@ -31,7 +33,7 @@ class EmailListRemoveHandler(CORSHandler):
         email = path.replace('/', '')
 
         if email and validate_email(email) and remove_from_email_list(email):
-            self.success(200)
+            self.success(200, 'Success')
         else:
             self.write_error(400)
         self.finish()
